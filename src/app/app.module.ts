c059713bdb54387d19648d4c7b6ac12d7f845597
vincentmuya <vincentmuya13@gmail.com>
2018-01-12 15:16:27 +0300

    modified the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule } from '@angular/common/http';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { RouterModule, Routes } from '@angular/router';
-import { NgProgressModule } from '@ngx-progressbar/core';
-// import { GithubComponent } from './github/github.component';
-
-const routes:Routes=[
-  {path:"about",component:AboutComponent},
-  {path:"contact",component:ContactComponent}
-]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    ContactComponent,
-    // GithubComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    NgProgressModule.forRoot(),
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule {
-gitName ='';
-gitHubDetails;
-name = '';
-id ='';
-url = '' ;
-avatar_url ='';
-
-
-}
+      BrowserModule,
+      HttpModule,
+      FormsModule
+    ],
+    providers: [HttpModule],
+      bootstrap: [AppComponent]
+    })
+    export class AppModule { }    
