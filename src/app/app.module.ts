477ebb139170fb0c8b3be58c7fcac5a533a774f1
vincentmuya <vincentmuya13@gmail.com>
2018-01-12 14:55:19 +0300

    modified code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -7,7 +8,7 @@ import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgProgressModule } from '@ngx-progressbar/core';
 // import { GithubComponent } from './github/github.component';
-import {HttpClientModule } from '@angular/common/http'
+
 const routes:Routes=[
   {path:"about",component:AboutComponent},
   {path:"contact",component:ContactComponent}
@@ -37,18 +38,5 @@ id ='';
 url = '' ;
 avatar_url ='';
 
-constructor (private http:Http) {}
 
-    searchGitName() {
-      this.http.get('https://api.github.com/users/' +this.gitName).subscribe(
-        (res:Response)=>{
-          const gitHubDetails1 =res.json();
-          console.log(gitHubDetails1)
-          this.gitHubDetails= gitHubDetails1;
-
-
-        }
-      )
-
-    }
 }
--- src/app/lookup-model.ts
+++
@@ -1,7 +0,0 @@
-export class gitHubDetails {
-  name: string;
-  avatar_url: string;
-  company: string;
-  id: number;
-  url: string;
-}
