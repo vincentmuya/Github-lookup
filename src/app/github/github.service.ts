be15b301fd1c3c8eb25c22f1c4f006d68c7c3094
vincentmuya <vincentmuya13@gmail.com>
2018-01-13 12:06:42 +0300

    added the map function and imported it in the rxjs/add/operator/map

--- src/app/github/github.service.ts
+++ src/app/github/github.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders} from '@angular/common/http';
 import { Details } from '../details'
-import 'rxjs/add/operator/';
+import 'rxjs/add/operator/map';
 @Injectable()
 export class GithubService {
-getDetails(){
-  return Details;
-}
   constructor(private _http:HttpClient) { }
-
+  getDetails(){
+    return this._http.get("")
+    .map(result => result);
+}
 }
