e03aa2bfe38d2bd907e7b4f37e6c00ba323fab37
vincentmuya <vincentmuya13@gmail.com>
2018-01-13 11:54:41 +0300

    imported some modules to service

--- src/app/github/github.service.ts
+++ src/app/github/github.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
 import { Details } from '../details'
+import 'rxjs/add/operator/';
 @Injectable()
 export class GithubService {
 getDetails(){
